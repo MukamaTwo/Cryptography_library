7962c0df4afff5c0ec6c9ed138a6ff9872c2ada3
Josef M <joseph.mukama@gmail.com>
2016-12-04 04:23:57 +0100

    implemented the extended eucleadian algorithm

--- src/Cryptography_library/CryptoLib.java
+++ src/Cryptography_library/CryptoLib.java
@@ -19,6 +19,52 @@ public class CryptoLib {
 		int s = -1;
 		int t = -1;
 		int[] result = new int[3];
+		int r0, r1, s0, s1, t0, t1;
+		int r_prev, r_curr, r_next, q;   //for the equation
+		int s_prev,  s_next ;
+		int t_prev,  t_next ;
+		
+		//ensure that the first term is bigger
+		r0=(a>b)?a:b;
+		r1=(a>b)?b:a;
+		q=r0/r1;     
+				
+		//needed to enter the loop
+		r_next=r0-q*r1;
+		r_curr=r1;
+		
+		//s values
+		s0=(a>b)?1:0;
+		s1=(a>b)?0:1;
+		
+		s_prev=s0; 
+		s=s1;
+		s_next=s_prev-q*s;
+		
+		//t values
+		t0=(a>b)?0:1;
+		t1=(a>b)?1:0;		
+
+		t_prev=t0; 
+		t=t1;
+		t_next=t_prev-q*t;
+		
+		while(r_next>0 && r_next<Math.abs(r_curr)){				
+			r_prev=r_curr;
+			r_curr=r_next;
+			q=r_prev/r_curr;
+			r_next=r_prev-q*r_curr;	
+			
+			s_prev=s;
+			s=s_next;
+			s_next=s_prev-q*s;
+			
+			t_prev=t;
+			t=t_next;
+			t_next=t_prev-q*t;
+		}
+	
+		gcd=r_curr;
 		result[0] = gcd;
 		result[1] = s;
 		result[2] = t;
