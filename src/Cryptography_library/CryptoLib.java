ca2cba6d1e5fc7c73884d8c0210bb70371e7737b
Josef M <joseph.mukama@gmail.com>
2016-12-04 04:01:09 +0100

    initial project files

---
+++ src/Cryptography_library/CryptoLib.java
@@ -0,0 +1,61 @@
+package Cryptography_library;
+
+// Compilation (CryptoLibTest contains the main-method):
+//   javac CryptoLibTest.java
+// Running:
+//   java CryptoLibTest
+
+public class CryptoLib {
+
+	/**
+	 * Returns an array "result" with the values "result[0] = gcd",
+	 * "result[1] = s" and "result[2] = t" such that "gcd" is the greatest
+	 * common divisor of "a" and "b", and "gcd = a * s + b * t".
+	 **/
+	public static int[] EEA(int a, int b) {
+		// Note: as you can see in the test suite,
+		// your function should work for any (positive) value of a and b.
+		int gcd = -1;
+		int s = -1;
+		int t = -1;
+		int[] result = new int[3];
+		result[0] = gcd;
+		result[1] = s;
+		result[2] = t;
+		return result;
+	}
+
+	/**
+	 * Returns Euler's Totient for value "n".
+	 **/
+	public static int EulerPhi(int n) {
+		return -1;
+	}
+
+	/**
+	 * Returns the value "v" such that "n*v = 1 (mod m)". Returns 0 if the
+	 * modular inverse does not exist.
+	 **/
+	public static int ModInv(int n, int m) {
+		return -1;
+	}
+
+	/**
+	 * Returns 0 if "n" is a Fermat Prime, otherwise it returns the lowest
+	 * Fermat Witness. Tests values from 2 (inclusive) to "n/3" (exclusive).
+	 **/
+	public static int FermatPT(int n) {
+		return -1;
+	}
+
+	/**
+	 * Returns the probability that calling a perfect hash function with
+	 * "n_samples" (uniformly distributed) will give one collision (i.e. that
+	 * two samples result in the same hash) -- where "size" is the number of
+	 * different output values the hash function can produce.
+	 **/
+	public static double HashCP(double n_samples, double size) {
+		return -1;
+	}
+
+}
